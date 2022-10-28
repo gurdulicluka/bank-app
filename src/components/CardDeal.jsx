2f8d8d4a1ec5ac0ce7136922a6099d5724e4d9fa
Luka Gurdulic <gurdulicluka@gmail.com>
2022-10-28 16:02:54 +0200

    FEAT(CardDeal): Add card deal section component

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
 const CardDeal = () => {
-  return <div>CardDeal</div>;
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className="sm:block hidden" /> in few easy
+          steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[480px] mt-5`}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est itaque
+          doloribus nostrum sint accusamus et fugiat iusto nulla nemo quo.
+        </p>
+        <Button styles="mt-10" />
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="w-[100%] h-[100%]" />
+      </div>
+    </section>
+  );
 };
 
 export default CardDeal;
